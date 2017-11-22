bde1b6884068ac7f704afc8b0b860bea1562faef
Arthur <SilverFire098@gmail.com>
2017-11-22 09:25:56 +0200

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,59 @@
+import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;
+
+const splitTextToPages = (text) => {
+    let words = text.split(' ');
+    let splittedText = "", length = 0, rowText = "- ";
+
+    for(let i = 0; i < words.length; i++) {
+        if(words[i][words[i].length - 1] === ':') {
+            words[i] = words[i].slice(0, words[i].length - 1) + '.';
+            words[i + 1] ?
+                words[i + 1] = words[i + 1].charAt(0).toUpperCase() + words[i + 1].slice(1) : undefined;
+        }
+        if (words[i].length > 20) {
+            console.log("Skipped: ", words[i]);
+        } else if(words[i].length + length <= 230) {
+            rowText += words[i] + ' ';
+            length += words[i].length + 1;
+        } else {
+            splittedText += rowText + '\n';
+            rowText = "- ";
+            length = 0;
+            i--;
+        }
+    }
+    splittedText += rowText;
+
+    return splittedText;
+};
+
+const uploadFile = (data, name) => {
+    let a = document.getElementById('hidden-export'),
+        blob = new Blob([data], {type: "text/plain"}),
+        url = window.URL.createObjectURL(blob),
+        filename = name ? name + ".yml" : "book.yml";
+
+    if (window.navigator.msSaveOrOpenBlob)
+        window.navigator.msSaveOrOpenBlob(blob, filename);
+    else {
+        a.href = url;
+        a.target = '_blank';
+        a.download = filename;
+        a.click();
+        window.URL.revokeObjectURL(url);
+    }
+};
+
+$(document).ready (() => {
+    $("#export-button").on("click", (event) => {
+        event.preventDefault();
+        let filename = $("#file-name-input").val(),
+            title = $("#title-input").val(),
+            author = $("#author-input").val(),
+            text = $("#text-input").val();
+        let pages = splitTextToPages(text);
+        let book = `Title: ${title}\nAuthor: ${author}\nPages:\n${pages}`;
+        uploadFile(book, filename)
+    })
+});
